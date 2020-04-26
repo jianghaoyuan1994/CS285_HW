7852f599e25be177a340c0ee28bd528e83124f4b
jianghaoyuan <jianghaoyuan1994@gmail.com>
2020-04-26 22:13:36 +0800

    get ready to implement HW3

--- hw3/cs285/infrastructure/utils.py
+++ hw3/cs285/infrastructure/utils.py
@@ -1,24 +1,79 @@
 import numpy as np
 import time
 
+
 ############################################
 ############################################
 
-def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('rgb_array')):
+def sample_trajectory(env, policy, max_path_length, render=False, render_mode='rgb_array'):
+    # initialize env for the beginning of a new rollout
+    ob = env.reset()
 
-    # TODO: GETTHIS from HW2
+    # init vars
+    obs, acs, rewards, next_obs, terminals, image_obs = [], [], [], [], [], []
+    steps = 0
+    while True:
 
-def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, render=False, render_mode=('rgb_array')):
+        # render image of the simulated env
+        if render:
+            if 'rgb_array' in render_mode:
+                if hasattr(env, 'sim'):
+                    if 'track' in env.env.model.camera_names:
+                        image_obs.append(env.sim.render(camera_name='track', height=500, width=500)[::-1])
+                    else:
+                        image_obs.append(env.sim.render(height=500, width=500)[::-1])
+                else:
+                    image_obs.append(env.render(mode=render_mode))
+            if 'human' in render_mode:
+                env.render(mode=render_mode)
+                time.sleep(env.model.opt.timestep)
 
-    # TODO: GETTHIS from HW1 or HW2
+        # use the most recent ob to decide what to do
+        obs.append(ob)
+        ac = policy.get_action(ob)
+        ac = ac[0]
+        acs.append(ac)
+
+        # take that action and record results
+        ob, rew, done, _ = env.step(ac)
+
+        # record result of taking that action
+        steps += 1
+        next_obs.append(ob)
+        rewards.append(rew)
+
+        # End the rollout if the rollout ended
+        # Note that the rollout can end due to done, or due to max_path_length
+        rollout_done = (steps == max_path_length) or done
+        terminals.append(rollout_done)
+
+        if rollout_done:
+            break
+
+    return Path(obs, image_obs, acs, rewards, next_obs, terminals)
+
+
+def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, render=False, render_mode='rgb_array'):
+
+    timesteps_this_batch = 0
+    paths = []
+    while timesteps_this_batch < min_timesteps_per_batch:
+        thispath = sample_trajectory(env, policy, max_path_length, render, render_mode)
+        paths.append(thispath)
+        timesteps_this_batch += get_pathlength(thispath)
 
     return paths, timesteps_this_batch
 
+
 def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False, render_mode=('rgb_array')):
-    
-    # TODO: GETTHIS from HW1 or HW2
+
+    paths = []
+
+    for _ in range(ntraj):
+        paths.append(sample_trajectory(env, policy, max_path_length, render, render_mode))
 
     return paths
+
 
 ############################################
 ############################################
@@ -30,10 +85,10 @@ def Path(obs, image_obs, acs, rewards, next_obs, terminals):
     """
     if image_obs != []:
         image_obs = np.stack(image_obs, axis=0)
-    return {"observation" : np.array(obs, dtype=np.float32),
-            "image_obs" : np.array(image_obs, dtype=np.uint8),
-            "reward" : np.array(rewards, dtype=np.float32),
-            "action" : np.array(acs, dtype=np.float32),
+    return {"observation": np.array(obs, dtype=np.float32),
+            "image_obs": np.array(image_obs, dtype=np.uint8),
+            "reward": np.array(rewards, dtype=np.float32),
+            "action": np.array(acs, dtype=np.float32),
             "next_observation": np.array(next_obs, dtype=np.float32),
             "terminal": np.array(terminals, dtype=np.float32)}
 
@@ -51,6 +106,7 @@ def convert_listofrollouts(paths):
     concatenated_rewards = np.concatenate([path["reward"] for path in paths])
     unconcatenated_rewards = [path["reward"] for path in paths]
     return observations, actions, next_observations, terminals, concatenated_rewards, unconcatenated_rewards
+
 
 ############################################
 ############################################
